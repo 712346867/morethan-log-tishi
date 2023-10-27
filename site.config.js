2591d7ec9b634a8da6b8540cb5d2e351196bc474
小太阳昌仔 <35997541+712346867@users.noreply.github.com>
2023-10-27 21:26:28 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,30 +1,30 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "小太阳昌仔prompt",
-    image: "https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F31125823-7fe8-4cb9-bf03-a9607eb70b81%2Fff5aaa1f-152f-4c8f-a82c-296d52b64155%2F04.png?id=88b40fd2-0e9f-445b-893d-c2d391062edd&table=collection&spaceId=31125823-7fe8-4cb9-bf03-a9607eb70b81&width=60&userId=0651fdcf-5495-4221-9e56-72163badcd50&cache=v2", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    name: "小太阳昌仔",
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "INTJ+摩羯座",
+    bio: "平静的努力🔥，精致的执着✨",
+    email: "aixin2019@gmail.com",
+    WeChat: "",
+    github: "小太阳昌仔",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `MY BLOG`,
+      href: "https://blog.ssuncz.top",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "小太阳昌仔",
+    description: "welcome to 小太阳昌仔 Prompts & Tools!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://tools.ssuncz.top",
+  since: 2020, // If leave this empty, current year will be used.
   lang: "zh-CN", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
