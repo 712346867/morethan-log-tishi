3b2c2953501beed7e8ccc727c5f7a3f38bd61495
小太阳昌仔 <35997541+712346867@users.noreply.github.com>
2023-10-27 21:44:00 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -15,6 +15,14 @@ const CONFIG = {
       name: `MY BLOG`,
       href: "https://blog.ssuncz.top",
     },
+    {
+      name: `MY Prompts`,
+      href: "https://blog.ssuncz.top",
+    },
+    {
+      name: `MY Tools`,
+      href: "https://blog.ssuncz.top",
+    },
   ],
   // blog setting (required)
   blog: {
