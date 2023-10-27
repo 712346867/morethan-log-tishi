e6fb0a9888456445cfd21677e2a3df929d0bac7b
小太阳昌仔 <35997541+712346867@users.noreply.github.com>
2023-10-28 01:40:48 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -47,7 +47,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
@@ -55,7 +55,7 @@ const CONFIG = {
     },
   },
   cusdis: {
-    enable: false,
+    enable: true,
     config: {
       host: "https://cusdis.com",
       appid: "9b73f394-3307-4b52-a5b1-a4c3b1e2497c", // Embed Code -> data-app-id value
